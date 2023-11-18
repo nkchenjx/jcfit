7129c9ac267dc831824785d887de795dd63b9a74
Jixin Chen <40338321+nkchenjx@users.noreply.github.com>
2023-11-17 18:05:42 -0600

    Update jcfit_L2.m

--- jcfit_L2/jcfit_L2.m
+++ jcfit_L2/jcfit_L2.m
@@ -27,8 +27,10 @@
 % OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 % SOFTWARE.
 
-%% an example for triple exponential decay fitting of a single curve.
 %{
+
+%% an example for triple exponential decay fitting of a single curve.
+% this example takes 40 s on a Intel i7 cpu. No GPU or parallel funciton has been used yet.
 
 clear
 
@@ -56,9 +58,9 @@ y2 = y + noise2; % white noise with equal weight
 
 % figure; plot(x, y); hold on; plot(x, y1); plot(x, y2); 
 
-    
-    x = x; % a row vector
-    y = y2; % a row vector
+%% ------load raw data------------------    
+    x = x; % a  vector
+    y = y2; % a vector
     figure; plot(x,y); title('raw data');
     
     
@@ -81,6 +83,7 @@ y2 = y + noise2; % white noise with equal weight
     option.maxiteration = 50;  % number of iteration fixed, the fitting will stop either this iteration or convergence reached first 
     option.precision = 1E-3;  % best searching precision, recommend 1 decimal better than desired. e.g want 0.01, set to 0.001.
     option.convgtest = 1e-100; % difference between two iterations on the square difference between fitting and data.
+%    option.step = 0.5; %Super important for speed. Suggest 0.5. Can be 0.1-4;
 
     % ----------------Attn: change below for different fitting equations-----------------
     % set the fitting equation to double exponential decay with a base line
@@ -107,8 +110,8 @@ y2 = y + noise2; % white noise with equal weight
 
     d1 = paraGuess-bounds(1,:);
     d2 = bounds(2,:)-paraGuess;
-    if prod(d1)*prod(d2)<=0
-        display('WARNING: initial guess out of boundary');
+    if ~isempty(find([d1,d2]<=0))
+        display(['WARNING: initial guess out of boundary paraGuess #', num2str(find(d1<=0)), num2str(find(d2<=0))]);
     end
     %--------------END of fitting option setting, equation, initial guess,
     %              and guessed parameter boundaries.
@@ -117,16 +120,26 @@ y2 = y + noise2; % white noise with equal weight
     %------------------and start fitting:------------------------
      
     tic
-         [paraHist, parafinal, paraBounds_95, chisq, rsq] = jcfit_L2(mdl, x, y, paraGuess, bounds, option);
+         
+         Results = jcfit_L2(mdl, x, y, paraGuess, bounds, option);
+         paraHist = Results.paraHist;
+         parafinal = Results.parafinal;
+         paraBounds_95 = Results.paraBounds_95;
+         chisq = Results.chisq;
+         rsq  = Results.rsq;
+    % warning: the parameter 95% confidence lower and upper bounds are based on estimation of the local minimum,
+    % not considering global minimum and other local minima.
     toc
+
+    
 %     fprintf(['\n rsq = ', num2str(rsq), '\n']);
     % parafinal is the fitted results; yfit is the fittered curve; 
     % use residual = y-yfit; to get the residual
     % rsq: root mean sqare value best will be close to 1
     
     %--------- plot results -----------------
-    yfit = mdl(parafinal, x);
-    residual = y - yfit;
+    yfit = Results.yfit;
+    residual = Results.residual;
     figure; plot(x,y,'linewidth',1.5); hold on; plot(x,yfit,'linewidth',1.5); plot(x, residual,'linewidth',1.5);
     title(['rsq = ', num2str(rsq)]);
     ax = gca;
@@ -137,17 +150,24 @@ y2 = y + noise2; % white noise with equal weight
     ax.FontSize = 20;
     ax.FontWeight = 'Bold';
     
+    figure; plot(Results.errorHist);
+    title('error vs iteration = ');
+    ax = gca;
+    ax.LineWidth = 1.5;
+    ax.Box = 'on';
+    ax.TickLength = [0.02, 0.02];
+    ax.FontName = 'Arial';
+    ax.FontSize = 20;
+    ax.FontWeight = 'Bold';
     %-------------------------------------
-    % End. by Jixin Chen @ Ohio University
 
- 
 %}
 
 
 %% main function with data error bar not fitted
 % -------if mdl is a complicated function, remove it from here and add it
 % in the end as a separated function.
-function [paraHist, parafinal, paraBounds_95, chisq, rsq] = jcfit_L2(mdl, x, y, paraGuess, bounds, option)
+function Results = jcfit_L2(mdl, x, y, paraGuess, bounds, option)
     % load options
     if isfield(option, 'maxiteration')
         maxiteration = option.maxiteration;
@@ -172,17 +192,24 @@ function [paraHist, parafinal, paraBounds_95, chisq, rsq] = jcfit_L2(mdl, x, y, 
         step = 0.5; % spacing fraction. Spacing is 2^(step*i)*precision, i is the number of guessing point away from the initial guess.
     end
     
+    x = x(:);  y = y(:);  % force column vector for both x and y
+      % a vector. 2D, 3D data need to modify the fitting function to calculate model and residual correctly 
+      % or vectorize and modify the mdl function to calculate the y_guess correctly
+    
     y_guess = mdl(paraGuess, x);
     residual = y-y_guess;
-    minerrorlast = dot(residual, residual); %L2, least square
+    minerrorlast = dot(residual, residual)/length(y); %L2, least square
 %    minerrorlast = sum(abs(residual(:))); %L1, least absolute various
     
     para = paraGuess;
-    paraHist = zeros(maxiteration, length(paraGuess) + 1); % last one error score
+    paraHist = zeros(maxiteration, length(paraGuess)); % last one error score
+    errorHist = zeros(maxiteration, 1);
+    errorCount = 0;
     
      tic
      for iteration = 1:maxiteration % fixed number of iteration.
-         paraHist(iteration,:) = [para, minerrorlast]; 
+         paraHist(iteration,:) = para; 
+         errorHist(iteration, 1) = minerrorlast;
          
          fprintf('.');
          if rem(iteration, 100) == 0 % progressing indicator
@@ -217,11 +244,11 @@ function [paraHist, parafinal, paraBounds_95, chisq, rsq] = jcfit_L2(mdl, x, y, 
              para(i) = ps(ind(1)); % if there are multiple minuma, take the first, could be p.
          end
          %test if converged
-         if abs(minerror-minerrorlast)< convgtest; %convergence test positive
+         if abs(minerror-minerrorlast)< convgtest  %convergence test positive
              fprintf(['\n converged at iteration = ', num2str(iteration)]);
              break
          end
-         minerrorlast = minerror;
+         minerrorlast = minerror/length(y);
      end
     fprintf('\n');
     
@@ -245,7 +272,11 @@ function [paraHist, parafinal, paraBounds_95, chisq, rsq] = jcfit_L2(mdl, x, y, 
 %     ax.FontName = 'Arial';
 %     ax.FontSize = 20;
 %     ax.FontWeight = 'Bold';
-%--------end plotting figure    
+%--------end plotting figure 
+    yfit = mdl(parafinal, x);
+    residual = y - yfit;
+    Results = struct('paraHist', paraHist, 'parafinal', parafinal, 'paraBounds_95', paraBounds_95,...
+        'chisq', chisq, 'rsq', rsq, 'x', x, 'y', y, 'yfit', yfit, 'residual', residual, 'errorHist', errorHist);
 end
 
 %% find errors of the fitting
