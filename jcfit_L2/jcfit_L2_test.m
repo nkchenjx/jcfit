36fe55ea0ec3e0f87379d0bed1b5d2b7765304d5
Jixin Chen <40338321+nkchenjx@users.noreply.github.com>
2023-11-20 22:30:23 -0600

    Update jcfit_L2_test.m

--- jcfit_L2/jcfit_L2_test.m
+++ jcfit_L2/jcfit_L2_test.m
@@ -78,7 +78,7 @@ y2 = y + noise2; % white noise with equal weight
     % set fitting options
     option.maxiteration = 100;  % number of iteration fixed, the fitting will stop either this iteration or convergence reached first 
     option.precision = 1E-6;  % best searching precision, recommend 1 decimal better than desired. e.g want 0.01, set to 0.001.
-    option.convgtest = 1e-100; % difference between two iterations on the square difference between fitting and data.
+    option.convgtest = 0; % difference between two iterations on the square difference between fitting and data.
 %    option.step = 0.5; %Super important for speed. Suggest 0.5. Can be 0.1-4;
 
     % ----------------Attn: change below for different fitting equations-----------------
